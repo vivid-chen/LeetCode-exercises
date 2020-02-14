525f605826fdfab23944d628b2678c3004a108aa
CZY <1031268032@qq.com>
2020-02-14 13:54:49 +0800

    # 经典排序算法

---
+++ 归并排序递归实现.py
@@ -0,0 +1,54 @@
+def merge(arr, l, m, r): 
+    n1 = m - l + 1
+    n2 = r- m 
+  
+    # 创建临时数组
+    L = [0] * (n1)
+    R = [0] * (n2)
+  
+    # 拷贝数据到临时数组 arrays L[] 和 R[] 
+    for i in range(0 , n1): 
+        L[i] = arr[l + i] 
+  
+    for j in range(0 , n2): 
+        R[j] = arr[m + 1 + j] 
+  
+    # 归并临时数组到 arr[l..r] 
+    i = 0     # 初始化第一个子数组的索引
+    j = 0     # 初始化第二个子数组的索引
+    k = l     # 初始归并子数组的索引
+  
+    while i < n1 and j < n2 : 
+        if L[i] <= R[j]: 
+            arr[k] = L[i] 
+            i += 1
+        else: 
+            arr[k] = R[j] 
+            j += 1
+        k += 1
+  
+    # 拷贝 L[] 的保留元素
+    while i < n1: 
+        arr[k] = L[i] 
+        i += 1
+        k += 1
+  
+    # 拷贝 R[] 的保留元素
+    while j < n2: 
+        arr[k] = R[j] 
+        j += 1
+        k += 1
+  
+def mergeSort(arr,l,r): 
+    if l < r: 
+  
+        
+        m = int((l+(r-1))/2)
+  
+       
+        mergeSort(arr, l, m) 
+        mergeSort(arr, m+1, r) 
+        merge(arr, l, m, r)
+    return arr
+
+print(mergeSort([3,2,1],0,2))
