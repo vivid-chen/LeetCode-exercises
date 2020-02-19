74cee640e8ac631e7a0ec963da5950e5cf266e8e
CZY <1031268032@qq.com>
2020-02-19 15:37:49 +0800

    全排列去重复两种方法实现

--- 剑指offer/offer38字符串的排列.py
+++ 剑指offer/offer38字符串的排列.py
@@ -25,7 +25,49 @@ def dfs(Position,size,Flag,temp,res,string):
                 Flag[i] = False
                 dfs(Position+1,size,Flag,temp,res,string)
                 Flag[i] = True
+
+
+def Repeat(located,P):
+    global string
+    # 如果当前指到的前面出现过就不用考虑了
+    for i in range(located,P):
+        if string[i] == string[P]:
+            return False
+    return True
+
+def SwapPermutation(n):
+    global string
+    size = len(string)
+    if size <= 0:
+        return print("Para Error")
+    if n == size:
+        print(string)
+        res.append(string)
+        return
     
+    for i in range(n,size):
+        if Repeat(n,i):
+            # 交换
+            temp = string[n]
+            string[n] = string[i]
+            string[i] = temp
+            # 递归
+            SwapPermutation(n+1)
+            # 递归结束后还原
+            temp = string[n]
+            string[n] = string[i]
+            string[i] = temp
 
 
-Permutation("1223")
+
+if __name__ == "__main__":
+    Permutation("1223") # 全排列回溯加标记，效率低
+    
+    # 方法2，将string转化为list处理，因为python中不能修改str
+    # 后面如果有需要再转化为str
+    string = list("1223")
+    res = []
+    SwapPermutation(0)
+    print(len(res))
+
+
