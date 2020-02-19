0e48298952b203f95c21229cd0951fac34af0952
CZY <1031268032@qq.com>
2020-02-19 09:46:28 +0800

    # 对称二叉树判断&递归实现

---
+++ 剑指offer/offer28对称的二叉树.py
@@ -0,0 +1,74 @@
+class TreeNode(object):
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.val) # print一个TreeNode类会返回val
+    
+class tree:
+    def build_binary_tree(vals):
+        """
+        return the TreeNode root
+        :param vals:List
+        :return:tree
+        """
+        from queue import Queue
+
+        root = TreeNode(vals[0])
+        q = Queue()  # nodes that has been used to store a val, but its children has not been used.
+        q.put(root)
+
+        if len(vals) % 2 == 0: # 防止每次取2个数出现越界
+            vals += [None]
+        for val_index in range(1, len(vals), 2):  # 每次两个值添加到一个已经赋值的结点的左右结点中
+            node = q.get()
+            if vals[val_index] is not None:
+                node.left = TreeNode(vals[val_index])
+                q.put(node.left)
+            if vals[val_index + 1] is not None:
+                node.right = TreeNode(vals[val_index + 1])
+                q.put(node.right)
+        return root
+
+# 干脆打印一下看看树,打印顺序从上往下
+def PrintTreeFromTopToDown(root):
+    from queue import Queue
+    if root is None:
+        return print("This Tree is Empty!")
+    q = Queue()
+    q.put(root)
+    nextLevel = 0
+    toBePrinted = 1
+    while not q.empty():
+        Node = q.get()
+        print(Node.val,end=' ')
+
+        if Node.left is not None:
+            q.put(Node.left)
+            nextLevel += 1
+        if Node.right is not None:
+            q.put(Node.right)
+            nextLevel += 1
+        
+        toBePrinted -= 1
+        if toBePrinted == 0:
+            toBePrinted = nextLevel
+            nextLevel = 0
+            print('\n')
+
+def TreeIsSymmetrical(root1,root2):
+    if root1 is None and root2 is None:
+        return True
+    if root1 is None or root2 is None:
+        return False
+    if root1.val != root2.val:
+        return  False
+    return TreeIsSymmetrical(root1.left,root2.right) and TreeIsSymmetrical(root1.right,root2.left)
+
+
+
+root = tree.build_binary_tree([8,6,6,5,7,7,4])
+PrintTreeFromTopToDown(root)
+print(TreeIsSymmetrical(root,root))
