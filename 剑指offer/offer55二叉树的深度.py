6053e586e91746f0f5653ea31dc2627772f926b0
CZY <1031268032@qq.com>
2020-04-01 15:34:34 +0800

    后序非递归

--- 剑指offer/offer55二叉树的深度.py
+++ 剑指offer/offer55二叉树的深度.py
@@ -95,6 +95,28 @@ def inorder(root:TreeNode):
             pos = pos.right
     return res
 
+# 后序打印二叉树（非递归）
+# 使用两个栈结构
+# 第一个栈进栈顺序：左节点->右节点->根节点
+# 第一个栈弹出顺序： 根节点->右节点->左节点(先序遍历栈弹出顺序：根->右->左)
+# 第二个栈存储为第一个栈的每个弹出依次进栈
+# 最后第二个栈依次出栈
+def postOrderTraverse(root):
+    res = []
+    stack = [root]
+    stack2 = []
+    while len(stack) > 0:
+        node = stack.pop()
+        stack2.append(node)
+        if node.left is not None:
+            stack.append(node.left)
+        if node.right is not None:
+            stack.append(node.right)
+    while len(stack2) > 0:
+        # print(stack2.pop().val)
+        res.append(stack2.pop().val)
+    return res
+
 
 if __name__ == "__main__":
     root = tree.build_binary_tree([1,2,3,4,5,6,7])
